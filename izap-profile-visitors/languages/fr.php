25977178865d31aeabf9d2ba0c3a88258f3f09fd
Emmanuel <emanwebdev@gmail.com>
2012-05-27 18:30:46 +0200

    French language pack

---
+++ izap-profile-visitors/languages/fr.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * iZAP izap profile visitor French language pack
+ *
+ * @license GNU Public License version 3
+ * @author iZAP Team "<support@izap.in>"
+ * @link http://www.izap.in/
+ *
+ * iionly; Version 1.8:
+ * compatibility elgg-1.8
+ */
+
+$french = array(
+'izapProfileVisitor:Widget' => 'Profils des visiteurs récents',
+'izapProfileVisitor:WidgetDescription' => 'Ce widget vous permet de savoir qui a visité votre profil récemment.',
+'izapProfileVisitor:NumberOfVisitors' => 'Visiteurs ',
+'izapProfileVisitor:NoVisits' => ':( Personne n\'a encore visité mon profil.',
+);
+
+add_translation("fr",$french);
